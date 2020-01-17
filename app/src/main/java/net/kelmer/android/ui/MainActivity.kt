e0c654ad1b9deadff6a2a2a2978ac5af758afc28
kelmer <kelmer@gmail.com>
2020-01-17 11:46:45 +0100

    Searchbar as well

--- app/src/main/java/net/kelmer/android/ui/MainActivity.kt
+++ app/src/main/java/net/kelmer/android/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package net.kelmer.android.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -11,16 +13,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import net.kelmer.android.common.resolve
 import net.kelmer.android.flickrsearch.R
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
-    val photoAdapter = PhotoListAdapter()
+    private val photoAdapter = PhotoListAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         photolist_recyclerview.apply {
@@ -41,5 +43,36 @@ class MainActivity : AppCompatActivity() {
             )
         })
         viewModel.search("kittens")
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val searchItem =  menu.findItem(R.id.action_search)
+        // Optional: if you want to expand SearchView from icon to edittext view
+        searchItem.expandActionView()
+
+        val searchView = searchItem.actionView as SearchView
+        initSearch(searchView)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    private fun initSearch(searchView: SearchView) {
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                if(newText!=null && newText.length > 3) {
+                    viewModel.search(newText)
+                }
+                return true
+            }
+
+        })
+
     }
 }

7f6dfee85546ebe8fd820aa614eadc4134243cb8
kelmer <kelmer@gmail.com>
2020-01-18 12:52:59 +0100

    remove extra gson line

--- app/src/main/java/net/kelmer/android/data/model/PhotoListResponse.kt
+++ app/src/main/java/net/kelmer/android/data/model/PhotoListResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class PhotoListResponse(
     val page: Int,
     val pages: Int,
-    @SerializedName("perpage") val perPage: Int,
+    val perPage: Int,
     val total: Int,
     val photo: List<PhotoEntity>
 )
